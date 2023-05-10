45b4030a5136c22e511b5ff833cc505f74ccc2b1
DrTimLindsay <dr.tim.lindsay@gmail.com>
2023-05-09 20:30:21 -0400

    should work with notebook

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 import plotly.express as px
 import plotly.io as pio
 
-pio.renderers.default = "vscode"
+pio.renderers.default = "notebook"
 
 gapminder = px.data.gapminder()
-fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
-                 hover_name="country", log_x=True, size_max=60)
+fig = px.scatter(
+    gapminder.query("year==2007"),
+    x="gdpPercap",
+    y="lifeExp",
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=True,
+    size_max=60,
+)
 fig.show()
